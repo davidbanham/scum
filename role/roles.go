cced77df9a3bed17f9bd71e7418dba79c7f0d500
David Banham <david@banham.id.au>
2023-12-06 14:26:04 +1100

    Role over, implications and safer access

--- role/roles.go
+++ role/roles.go
@@ -8,7 +8,16 @@ import (
 
 type Roles []Role
 
-func (roles Roles) ByName() map[string]Role {
+func (roles Roles) ByName(name string) Role {
+	for _, r := range roles {
+		if r.Name == name {
+			return r
+		}
+	}
+	return Role{}
+}
+
+func (roles Roles) NamedMap() map[string]Role {
 	ret := map[string]Role{}
 	for _, r := range roles {
 		ret[r.Name] = r
@@ -38,23 +47,48 @@ func (roles Roles) Can(name string) bool {
 	return false
 }
 
-//func (roles Roles) CanOver(name string, entityID string) bool {
-//	for _, role := range roles {
-//		for _, sub := range role.Over {
-//			if sub == entityID {
-//				return role.Can(name)
-//			}
-//		}
-//	}
-//	return false
-//}
+func (roles Roles) CanOver(name string, entityID string) bool {
+	for _, role := range roles {
+		for _, sub := range role.Over {
+			if sub == entityID {
+				return role.Can(name)
+			}
+		}
+	}
+	return false
+}
+
+func (roles *Roles) AssignEntities(name string, entityIDs []string) {
+	for i, role := range *roles {
+		if role.Name == name {
+			(*roles)[i].Over = entityIDs
+		}
+	}
+}
+
+func (this *Roles) Implications(validRoles Roles) {
+	for i, role := range *this {
+		role.Implies = validRoles.ByName(role.Name).Implies
+		for j, sub := range role.Implies {
+			sub.Implications(validRoles)
+			role.Implies[j] = sub
+		}
+		(*this)[i] = role
+	}
+}
 
 type Role struct {
-	Name    string `json:"name"`
-	Label   string `json:"label"`
-	Implies Roles  `json:"-"`
+	Name    string   `json:"name"`
+	Label   string   `json:"label"`
+	Implies Roles    `json:"-"`
+	Over    []string `json:"over"`
+}
 
-	//Over       []string `json:"over"`
+func (this *Role) Implications(validRoles Roles) {
+	this.Implies = validRoles.ByName(this.Name).Implies
+	for _, role := range this.Implies {
+		role.Implications(validRoles)
+	}
 }
 
 func (this *Role) Can(role string) bool {
