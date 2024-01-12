10248408d96fa7978f9b4ca9aa5ef610522683d5
David Banham <david@banham.id.au>
2024-01-12 17:56:47 +1100

    PrevDay util

--- util/week_boundaries.go
+++ util/week_boundaries.go
@@ -10,3 +10,10 @@ func NextDay(begin time.Time, target time.Weekday) time.Time {
 	}
 	return NextDay(begin.Add(24*time.Hour), target)
 }
+
+func PrevDay(begin time.Time, target time.Weekday) time.Time {
+	if begin.Weekday() == target {
+		return begin
+	}
+	return NextDay(begin.Add(-24*time.Hour), target)
+}
